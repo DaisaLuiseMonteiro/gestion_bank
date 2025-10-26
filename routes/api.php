b39edcb5a5af20302a2981edfb3359c5378a70cd
Daisa Luise Monteiro <louise2002yaho@gmail.com>
2025-10-26 16:44:50 +0000

    miuse a jour

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompteController;
+use App\Http\Controllers\ClientController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::prefix('monteiro.daisa/v1')->group(function () {
+    // Comptes
+    Route::get('comptes', [CompteController::class, 'index']);
+    Route::get('clients/{clientId}/comptes', [CompteController::class, 'byClient']);
+
+    // Clients
+    Route::get('clients', [ClientController::class, 'index']);
+});
