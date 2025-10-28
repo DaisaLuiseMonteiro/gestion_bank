ac97bcc4fbad297a00109042d460d8ed6f5a90af
Daisa Luise Monteiro <louise2002yaho@gmail.com>
2025-10-28 20:41:56 +0000

    mise a jour

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompteController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\CompteBloqueController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +22,23 @@ Route::prefix('monteiro.daisa/v1')->middleware('logging')->group(function () {
     Route::get('clients/{clientId}/comptes', [CompteController::class, 'byClient']);
     Route::get('comptes/{compteId}', [CompteController::class, 'show']);
     Route::patch('comptes/{compteId}', [CompteController::class, 'update']);
-
+    
+    // Gestion du blocage des comptes
+    Route::post('comptes/{compteId}/bloquer', [CompteBloqueController::class, 'bloquer'])
+        ->middleware('auth:api');
+    Route::post('comptes/{compteId}/debloquer', [CompteBloqueController::class, 'debloquer'])
+        ->middleware('auth:api');
 
     // Clients
     Route::get('clients', [ClientController::class, 'index']);
 });
+
+// Documentation Swagger
+/**
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     securityScheme="bearerAuth",
+ *     bearerFormat="JWT"
+ * )
+ */
