5dfec412f24bf3ba87b2135e15c06cc9ba15bae6
Daisa Luise Monteiro <louise2002yaho@gmail.com>
2025-10-29 14:24:48 +0000

    mise a jour

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompteController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\CompteBloqueController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,16 +12,21 @@ use App\Http\Controllers\ClientController;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assigned to the "api" middleware group.
 |
 */
 
 Route::prefix('monteiro.daisa/v1')->middleware('logging')->group(function () {
-    // Comptes
+    // Routes de lecture (GET)
     Route::get('comptes', [CompteController::class, 'index']);
-    Route::get('clients/{clientId}/comptes', [CompteController::class, 'byClient']);
     Route::get('comptes/{compteId}', [CompteController::class, 'show']);
-
-    // Clients
     Route::get('clients', [ClientController::class, 'index']);
+    Route::get('clients/{clientId}/comptes', [CompteController::class, 'byClient']);
+    
+    // Routes de modification (POST/PATCH/DELETE)
+    Route::post('comptes', [CompteController::class, 'store']);
+    Route::patch('comptes/{compteId}', [CompteController::class, 'update']);
+    Route::delete('comptes/{compteId}', [CompteController::class, 'destroy']);
+    Route::post('comptes/{compteId}/bloquer', [CompteBloqueController::class, 'bloquer']);
+    Route::post('comptes/{compteId}/debloquer', [CompteBloqueController::class, 'debloquer']);
 });
